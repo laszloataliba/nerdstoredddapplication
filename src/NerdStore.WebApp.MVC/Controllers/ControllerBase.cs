d1a831a03e783a913b818eb30ef41ae96c3cf694
laszloataliba <laszloataliba@gmail.com>
2022-03-15 00:57:32 -0300

    Implementacao mediator

---
+++ src/NerdStore.WebApp.MVC/Controllers/ControllerBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.MVC.Controllers
+{
+    public abstract class ControllerBase : Controller
+    {
+        protected Guid ClienteId = Guid.NewGuid();
+    }
+}

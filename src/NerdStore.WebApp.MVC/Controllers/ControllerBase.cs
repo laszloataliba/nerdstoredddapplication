0d94c910ecdc013fdf995ea9caa7ef1066bd1b70
laszloataliba <laszloataliba@gmail.com>
2022-03-16 00:40:38 -0300

    Problemas com DI

--- src/NerdStore.WebApp.MVC/Controllers/ControllerBase.cs
+++ src/NerdStore.WebApp.MVC/Controllers/ControllerBase.cs
@@ -8,6 +8,6 @@ namespace NerdStore.WebApp.MVC.Controllers
 {
     public abstract class ControllerBase : Controller
     {
-        protected Guid ClienteId = Guid.NewGuid();
+        protected Guid ClienteId = Guid.Parse("b32af718-cf08-476d-995c-bb586b64bee4");
     }
 }
